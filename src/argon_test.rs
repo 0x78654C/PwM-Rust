8b96ca772dfa89cd491b6bc28aeb5e5a130cdaf9
xeL <l2xguard@live.com>
2023-06-25 23:43:42 +0300

    created argon2id tests

---
+++ src/argon_test.rs
@@ -0,0 +1,39 @@
+#[cfg(test)]
+ //extern crate argon2;
+use argon2::{self, Config, ThreadMode, Variant, Version};
+use base64::{encode};
+
+#[test]
+fn password_match_simple_test(){
+    let password = b"password";
+    let salt = b"radonmsalt";
+    let config = Config::default();
+    let hash = argon2::hash_encoded(password,salt , &config).unwrap();
+    let matches = argon2::verify_encoded(&hash,password).unwrap(); 
+    assert!(matches);
+}
+#[test]
+fn password_match_pwm(){
+    let password = b"@#$ASDSA#$#@34234asdASDas";
+    let hash_result = "p0XtEZ3yARyE0CfS+9nzZW17udJOTxJQRDdWSIMVnVA=";
+    let salt = b"a123@23sdasd";
+
+
+let config = Config {
+    variant: Variant::Argon2id,
+    version: Version::Version13,
+    mem_cost: 4096,
+    time_cost: 40,
+    lanes: 2,
+    thread_mode: ThreadMode::Parallel,
+    secret: &[],
+    ad: &[],
+    hash_length: 16
+};
+let hash = argon2::hash_encoded(password, salt, &config).unwrap();
+let hash_enc = encode(hash);
+let pass_enc = 
+println!("{}",hash_enc);
+//let matches = argon2::verify_encoded(&hash, password).unwrap();
+assert_eq!(hash_enc,hash_result);
+}
