1a850dfc70b8579c1d2c11ab2c5ed5dcd2adfa1a
xeL <l2xguard@live.com>
2023-06-26 23:17:49 +0300

    added substring for salt

--- src/argon_test.rs
+++ src/argon_test.rs
@@ -1,7 +1,8 @@
 #[cfg(test)]
- //extern crate argon2;
+//extern crate argon2;
 use argon2::{self, Config, ThreadMode, Variant, Version};
-use base64::{encode};
+use base64::encode;
+
 
 #[test]
 fn password_match_simple_test(){
@@ -12,28 +13,27 @@ fn password_match_simple_test(){
     let matches = argon2::verify_encoded(&hash,password).unwrap(); 
     assert!(matches);
 }
+
 #[test]
-fn password_match_pwm(){
-    let password = b"@#$ASDSA#$#@34234asdASDas";
+fn password_match_pwm() {
+    let password = "@#$ASDSA#$#@34234asdASDas";
+    let pass_substring = &password[2..10];
     let hash_result = "p0XtEZ3yARyE0CfS+9nzZW17udJOTxJQRDdWSIMVnVA=";
-    let salt = b"a123@23sdasd";
+    let salt = pass_substring.as_bytes();
+    let config = Config {
+        variant: Variant::Argon2id,
+        version: Version::Version13,
+        mem_cost: 4096,
+        time_cost: 40,
+        lanes: 2,
+        thread_mode: ThreadMode::Parallel,
+        secret: &[],
+        ad: &[],
+        hash_length: 16,
+    };
+    let hash = argon2::hash_encoded(password.as_bytes(), salt, &config).unwrap();
+    let hash_enc = encode(hash);
 
-
-let config = Config {
-    variant: Variant::Argon2id,
-    version: Version::Version13,
-    mem_cost: 4096,
-    time_cost: 40,
-    lanes: 2,
-    thread_mode: ThreadMode::Parallel,
-    secret: &[],
-    ad: &[],
-    hash_length: 16
-};
-let hash = argon2::hash_encoded(password, salt, &config).unwrap();
-let hash_enc = encode(hash);
-let pass_enc = 
-println!("{}",hash_enc);
-//let matches = argon2::verify_encoded(&hash, password).unwrap();
-assert_eq!(hash_enc,hash_result);
+    //let matches = argon2::verify_encoded(&hash, password).unwrap();
+    assert_eq!(hash_enc, hash_result);
 }
