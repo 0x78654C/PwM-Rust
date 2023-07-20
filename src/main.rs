7af3905fa87fa42aa4cfacecf04efe82a09fc2a0
xeL <l2xguard@live.com>
2023-07-20 22:03:32 +0300

    added function for create vaults

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,20 @@
 use std::env;
+use std::io::{stdin, stdout, Write};
+use std::fs;
+use std::fs::File;
+use std::io::prelude::*;
+use std::path::Path;
 use argon2::{self, Config, ThreadMode, Variant, Version};
-use base64::encode;
 mod aes_test;
 mod argon_test;
-//mod aes_test;
+
+#[path="./libs/argon_lib.rs"]
+mod argon_lib;
+
+#[path="./libs/aes_lib.rs"]
+mod aes_lib;
+
+const VAULT_DIR: &str = "Vaults";
 const HELP_MESSAGE: &str = "\
 PwM Copyright @ 2020-2022 0x078654c
 PwM - A simple password manager to store localy the authentification data encrypted for a application using Rijndael AES-256 and Argon2 for password hash.
@@ -35,11 +46,60 @@ impl Globals{
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-       
-    let mut VAULT_DIR:String = "".to_string();
     let arg1 = &args[1];
-    println!("{}", arg1);
+    if arg1 == "-h" {
+        println!("{}", HELP_MESSAGE);
+    }
+
+    if arg1 == "-createv" {
+        create_vault();
+    }
 }
+
+fn create_vault(){
+    let mut vault_name = String::new();
+    let mut master_password1 = String::new();
+    let mut master_password2 = String::new();
+    println!("{}", "Enter vault name:");
+    stdin().read_line(&mut vault_name);
+    if vault_name.len() < 3{
+        println!("{}", "Vault name must be at least 3 characters long!");
+        return;
+    }
+    println!("{}", "Master Password: ");
+    stdin().read_line(&mut master_password1);
+    if master_password1.len() < 12{
+        println!("{}", "Password must be at least 12 characters, and must include at least one upper case letter, one lower case letter, one numeric digit, one special character and no space!!");
+        return;
+    }
+    println!("{}", "Confirm Master Password: ");
+    stdin().read_line(&mut master_password2);
+
+     if master_password1 != master_password2{
+        println!("{}", "Passwords are not the same!");
+     }else{
+        let hash = argon_lib::argon_password_hash(master_password1.as_str());
+        let data = aes_lib::encrypt("".as_bytes(), hash.as_str());
+        let dir_exist:bool = Path::new(VAULT_DIR).is_dir();
+        if !dir_exist{
+            let _ =fs::create_dir(VAULT_DIR); 
+        }
+        let sep = "\\";
+        let mut vault_dir = VAULT_DIR.to_string();
+        vault_dir.to_string().push_str(sep);
+        vault_dir.to_string().push_str(vault_name.as_str());
+        let vault_exist: bool = Path::new(vault_name.as_str()).is_file();
+        println!("{}", vault_exist);
+        if !vault_exist{
+            println!("{}",vault_name);
+            let path = vault_name.trim();
+            let mut file =  File::create(path).expect("File exist?");
+            let _ = file.write_all(data.as_bytes());
+            println!("{}","Vault was created!");
+        }
+     }
+}
+
 
 // Check maximum  of tries. used in while loops for exit them at a certain count.
 fn check_max_tries()->bool{
