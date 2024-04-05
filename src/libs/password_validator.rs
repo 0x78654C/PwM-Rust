878e8f1f7810f2871e2281e6732135dc9f0d7e3c
0x78654c <xcoding.dev@gmail.com>
2024-04-05 16:08:16 +0300

    removed unused function

--- src/libs/password_validator.rs
+++ src/libs/password_validator.rs
@@ -5,8 +5,3 @@ pub fn validate_password (password:String)->bool{
     let re = Regex::new(r"^(\d)([a-z])([A-Z]).{12,500}$").unwrap();
      re.is_match(&password)
 }
-
-// Check if contains empty space.
-fn check_space_char(input:String)->bool{
-    input.contains(" ")
-}
