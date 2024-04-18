06123d770049f5417df516a76e55e208fad8722f
xeL <xcoding.dev@gmail.com>
2024-04-18 21:53:58 +0300

    new regex algorithm added

--- src/libs/password_validator.rs
+++ src/libs/password_validator.rs
@@ -2,6 +2,6 @@ use regex::Regex;
 
 // Validate password.
 pub fn validate_password (password:String)->bool{
-    let re = Regex::new(r"^(\d)([a-z])([A-Z]).{12,500}$").unwrap();
+    let re = Regex::new(r"^(?<=(.*[a-z]){3,})(?<=(.*[A-Z]){1,})(?<=(.*[0-9]){2,})(?<=(.*[!@#$%^&*()\-__+.]){1,}).{12,134}$").unwrap();
      re.is_match(&password)
 }
