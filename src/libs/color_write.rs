8b8992bdeaba12cf0eb851a02d1463e699edfa5f
xeL <xcoding.dev@gmail.com>
2024-04-24 22:37:12 +0300

    added cyan color

--- src/libs/color_write.rs
+++ src/libs/color_write.rs
@@ -5,26 +5,35 @@ use termcolor::{Color, ColorChoice, ColorSpec, StandardStream, WriteColor};
 // Write text in yellow.
 pub fn write_yellow(text:String){
     let mut stdout = StandardStream::stdout(ColorChoice::Always);
-    stdout.set_color(ColorSpec::new().set_fg(Some(Color::Yellow)));
+    let _ = stdout.set_color(ColorSpec::new().set_fg(Some(Color::Yellow)));
     writeln!(&mut stdout, "{}", text);
-    stdout.set_color(ColorSpec::new().set_fg(Some(Color::White)));
+    let _ = stdout.set_color(ColorSpec::new().set_fg(Some(Color::White)));
     writeln!(&mut stdout, "{}", "");
 }
 
 // Write text in green.
 pub fn write_green(text:String){
     let mut stdout = StandardStream::stdout(ColorChoice::Always);
-    stdout.set_color(ColorSpec::new().set_fg(Some(Color::Green)));
+    let _ = stdout.set_color(ColorSpec::new().set_fg(Some(Color::Green)));
     writeln!(&mut stdout, "{}", text);
-    stdout.set_color(ColorSpec::new().set_fg(Some(Color::White)));
+    let _ = stdout.set_color(ColorSpec::new().set_fg(Some(Color::White)));
     writeln!(&mut stdout, "{}", "");
 }
 
 // Write text in red.
 pub fn write_red(text:String){
     let mut stdout = StandardStream::stdout(ColorChoice::Always);
-    stdout.set_color(ColorSpec::new().set_fg(Some(Color::Red)));
+    let _ = stdout.set_color(ColorSpec::new().set_fg(Some(Color::Red)));
     writeln!(&mut stdout, "{}", text);
-    stdout.set_color(ColorSpec::new().set_fg(Some(Color::White)));
+    let _ = stdout.set_color(ColorSpec::new().set_fg(Some(Color::White)));
+    writeln!(&mut stdout, "{}", "");
+}
+
+// Write text in cyan.
+pub fn write_cyan(text:String){
+    let mut stdout = StandardStream::stdout(ColorChoice::Always);
+    let _ = stdout.set_color(ColorSpec::new().set_fg(Some(Color::Cyan)));
+    writeln!(&mut stdout, "{}", text);
+    let _ =  stdout.set_color(ColorSpec::new().set_fg(Some(Color::White)));
     writeln!(&mut stdout, "{}", "");
 }
