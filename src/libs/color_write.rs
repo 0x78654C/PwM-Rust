5dc01f279012828d5d7a5e2f2d94972a4d05026f
0x78654c <xcoding.dev@gmail.com>
2024-04-28 22:07:05 +0300

    removed unused color

--- src/libs/color_write.rs
+++ src/libs/color_write.rs
@@ -11,15 +11,6 @@ pub fn write_yellow(text:String){
     writeln!(&mut stdout, "{}", "");
 }
 
-// Write text in green.
-pub fn write_green(text:String){
-    let mut stdout = StandardStream::stdout(ColorChoice::Always);
-    let _ = stdout.set_color(ColorSpec::new().set_fg(Some(Color::Green)));
-    writeln!(&mut stdout, "{}", text);
-    let _ = stdout.set_color(ColorSpec::new().set_fg(Some(Color::White)));
-    writeln!(&mut stdout, "{}", "");
-}
-
 // Write text in red.
 pub fn write_red(text:String){
     let mut stdout = StandardStream::stdout(ColorChoice::Always);
